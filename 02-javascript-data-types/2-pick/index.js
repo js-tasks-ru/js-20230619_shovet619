3bea66d87093ec099bd6ff924f13a368c1b4a183
Nikita <shovet619@gmail.com>
2023-06-24 18:14:42 +0300

    2-pick and 3-omit solved

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const result = {};
+  fields.forEach((key) => {
+    result[key] = obj[key];
+  });
+  return result;
 };
