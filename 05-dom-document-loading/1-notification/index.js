a728a4642f23aafe6e6ab3c9b4c86d380994e954
Nikita <shovet619@gmail.com>
2023-07-05 16:48:57 +0300

    dom-document-loading

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,47 @@
 export default class NotificationMessage {
+  element = null
 
+  constructor(message = "", options = {}) {
+    const {duration = 1000, type = "success"} = options;
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {
+    this.element = this.createMessageElement(this.message, this.type, this.duration);
+  }
+
+  show(targetElement = document.body) {
+    targetElement.append(this.element);
+    this.remove();
+  }
+
+  destroy() {
+    this.element = null;
+  }
+
+  remove() {
+    setTimeout(() => {
+      this.element.remove();
+    }, this.duration);
+  }
+
+  createMessageElement(message, type, duration) {
+    const template = `
+        <div class="notification ${type}" style="--value:${duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+        <div class="notification-header">${type}</div>
+        <div class="notification-body">
+            ${message}
+        </div>
+        </div>
+    </div>
+    `;
+    const messageElement = document.createElement('template');
+    messageElement.innerHTML = template.trim();
+    return messageElement.content.firstChild;
+  }
 }
